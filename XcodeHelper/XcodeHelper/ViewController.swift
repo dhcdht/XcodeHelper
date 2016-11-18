951220506e68f59d25aca44e4a01225e752da69c
XieYajie <xyjxyf@163.com>
2016-11-18 12:24:06 +0800

    更新UI

--- XcodeHelper/XcodeHelper/ViewController.swift
+++ XcodeHelper/XcodeHelper/ViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet private var textView: NSTextView?
     var pluginWC = PluginWindowController(windowNibName: "PluginWindowController")
     var unsignXcodeWC = UnsignWindowController(windowNibName: "UnsignWindowController")
 
@@ -17,6 +18,9 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.textView?.isEditable = false
+        self.textView?.font = NSFont.systemFont(ofSize: 16)
+        self.textView?.string = "\n该软件用于管理Xcode插件和主题。\n\n如果您只想管理主题，不需要破解Xcode; \n\n如果您想管理插件，首先需要破解Xcode, 本软件会自动安装插件到破解的Xcode，您需要使用破解的Xcode才能调用安装的插件\n"
     }
 
     @IBAction func pluginButtonTapped(sender: AnyObject) -> Void {
