79a2eb8bd465574b089c3ad75eb3026bf4e8f7bc
dhcdht <dhcdht@qq.com>
2016-12-02 04:31:06 -0800

    更改引导描述

--- XcodeHelper/XcodeHelper/ViewController.swift
+++ XcodeHelper/XcodeHelper/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: NSViewController, SUUpdaterDelegate {
         // Do any additional setup after loading the view.
         self.textView?.isEditable = false
         self.textView?.font = NSFont.systemFont(ofSize: 16)
-        self.textView?.string = "\n该软件用于管理Xcode插件和主题。\n\n如果您只想管理主题，不需要破解Xcode; \n\n如果您想管理插件，首先需要破解Xcode, 本软件会自动安装插件到破解的Xcode，您需要使用破解的Xcode才能调用安装的插件\n"
+        self.textView?.string = "\n该软件用于管理Xcode插件和主题。\n\n如果您只想管理主题，不需要破解Xcode; \n\n如果您想管理插件，首先需要破解Xcode, 本软件会自动安装插件到破解的Xcode，您需要使用破解的Xcode才能调用安装的插件\n\n生成类图功能使用 Xcode 编译系统生成的中间文件导出了依赖关系图，对于大型项目，可以在导出类图后提前 filter，避免结点过多导致不可操作\n"
 
         // TODO: 暂时放到这里，自动升级安装的插件，以免有插件因为版本问题用不了
         AutoUpdateDVTPlugInCompatibilityUUID.shared.startChecking()
